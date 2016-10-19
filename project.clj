9030efda73cf2e5191f6d1763fbfd07e0778b32e
Mark Bastian <markbastian@gmail.com>
2016-10-18 23:36:41 -0600

    Project rename.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject l1-getting-started "0.1.0-SNAPSHOT"
+(defproject clojure-intro "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/getting_started/ch1_data.clj
+++ src/getting_started/ch1_data.clj
@@ -1,1 +1,17 @@
 (ns getting-started.ch1-data)
+
+
+
+;Vectors are like arrays
+[]
+[1 2 3 4 5 6]
+
+;Maps are
+{}
+{ :name "Mark" :year 2016 }
+
+#{}
+#{ 1 2 3 4 5 }
+
+()
+'(1 2 3 4 5)
